33d582b0eb11abcb9c0824894df9aacdb29c7846
estrizhnev <estrizhnev@yandex-team.ru>
2022-04-15 00:08:25 +0000

    fix style
    X-Arc-Original-Commit: 3bf827cc29ad6d7170aa25bce22f18a37a997636

--- chadmin/cli/restore_replica.py
+++ chadmin/cli/restore_replica.py
@@ -1,4 +1,4 @@
-from click import command, pass_context
+from click import command, option, pass_context
 
 from cloud.mdb.clickhouse.tools.chadmin.cli import execute_query
 
@@ -8,17 +8,17 @@ from cloud.mdb.clickhouse.tools.chadmin.cli import execute_query
 @pass_context
 def restore_replica_command(ctx, on_cluster):
     query = """
-         select database, name 
-         from system.tables 
+         select database, name
+         from system.tables
          where database not in ('system') and engine like '%Replicated%'
     """
     tables = execute_query(ctx, query, format='JSON')['data']
     for table in tables:
         print("Database - {0}. Table - {1}".format(table['database'], table['name']))
         restore_query = """
-            SYSTEM RESTORE REPLICA {0}.`{1}` 
+            SYSTEM RESTORE REPLICA {{ database }}.`{{ table }}`
             {% if on_cluster %}
                 ON CLUSTER '{{cluster}}'
             {% endif %}
-        """.format(table['database'], table['name'])
-        print(execute_query(ctx, restore_query, on_cluster=on_cluster, format=None))
+        """
+        print(execute_query(ctx, restore_query, database=table['database'], table=table['name'], on_cluster=on_cluster, format=None))
