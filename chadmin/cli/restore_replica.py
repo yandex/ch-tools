e57abf861b883e1910fbe595965c84ee3ee62dea
estrizhnev <estrizhnev@yandex-team.ru>
2022-04-18 21:09:54 +0000

    fix comments
    X-Arc-Original-Commit: 8a8d2a0fab25e638e2fec34857c7778380c5ef61

--- chadmin/cli/restore_replica.py
+++ chadmin/cli/restore_replica.py
@@ -1,24 +1,26 @@
 from click import command, option, pass_context
 
-from cloud.mdb.clickhouse.tools.chadmin.cli import execute_query
+from cloud.mdb.clickhouse.tools.chadmin.cli import execute_query, get_cluster_name
 
 
 @command('restore-replica')
-@option('--on_cluster', is_flag=True, help='Run RESTORE REPLICA on cluster ')
+@option('--cluster', '--on-cluster', 'on_cluster', is_flag=True, help='Run RESTORE REPLICA on cluster ')
 @pass_context
 def restore_replica_command(ctx, on_cluster):
     query = """
-         select database, name
-         from system.tables
-         where database not in ('system') and engine like '%Replicated%'
+         SELECT database, name
+         FROM system.tables
+         WHERE engine LIKE '%Replicated%'
     """
     tables = execute_query(ctx, query, format='JSON')['data']
     for table in tables:
         print("Database - {0}. Table - {1}".format(table['database'], table['name']))
+        cluster = get_cluster_name(ctx) if on_cluster else None
         restore_query = """
-            SYSTEM RESTORE REPLICA {{ database }}.`{{ table }}`
+            SYSTEM RESTORE REPLICA `{{ database }}`.`{{ table }}`
             {% if on_cluster %}
-                ON CLUSTER '{{cluster}}'
+                ON CLUSTER '{{ cluster }}'
             {% endif %}
         """
-        print(execute_query(ctx, restore_query, database=table['database'], table=table['name'], on_cluster=on_cluster, format=None))
+        print(execute_query(ctx, restore_query,
+              database=table['database'], table=table['name'], on_cluster=on_cluster, cluster=cluster, format=None))
