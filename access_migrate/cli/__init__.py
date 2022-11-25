e55e1c06469b1d150204337fe9395a0fd960c7db
ekopanev <ekopanev@yandex-team.ru>
2022-11-25 09:12:02 +0000

    Removed all version of CH migration tool
    X-Arc-Original-Commit: 50f86130363ce4ad3054265b13c20db949ab67d6

--- access_migrate/cli/__init__.py
+++
@@ -1,7 +0,0 @@
-from cloud.mdb.clickhouse.tools.access_migrate.cli.local_to_replicated import local_to_replicated
-from cloud.mdb.clickhouse.tools.access_migrate.cli.replicated_to_local import replicated_to_local
-
-__all__ = (
-    local_to_replicated,
-    replicated_to_local,
-)
--- access_migrate/cli/local_to_replicated.py
+++
@@ -1,8 +0,0 @@
-import click
-
-
-@click.command('local')
-@click.pass_context
-def local_to_replicated(ctx):
-    # todo: https://st.yandex-team.ru/MDB-20555
-    pass
