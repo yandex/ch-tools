e309c8dc40895e38e804a2c46ea4bb8f6b916d63
egor-medvedev <egor-medvedev@yandex-team.ru>
2022-12-07 19:48:07 +0000

    MDB-20694: ClickHouse: script for cleaning unused data directories
    X-Arc-Original-Commit: e931f88aa839aa910a839d5b2b6342795edb5927

---
+++ chadmin/cli/data_store_group.py
@@ -0,0 +1,101 @@
+from click import group, option
+import os
+import subprocess
+import shutil
+
+
+CLICKHOUSE_PATH = '/var/lib/clickhouse'
+CLICKHOUSE_STORE_PATH = CLICKHOUSE_PATH + '/store'
+
+
+@group('data-store')
+def data_store_group():
+    """
+    Commands for manupalating data stored by ClickHouse.
+    """
+    pass
+
+
+@data_store_group.command('clean-orphaned-tables')
+@option(
+    '--column',
+    'column',
+    default='',
+    help='Additional check: specified COLUMN name should exists in data to be removed. Example: `initial_query_start_time_microseconds.bin` for `query_log`-table.',
+)
+@option('--remove', is_flag=True, default=False, help='Flag to REMOVE data from store subdirectories.')
+def clean_orphaned_tables_command(column, remove):
+    for prefix in os.listdir(CLICKHOUSE_STORE_PATH):
+        path = CLICKHOUSE_STORE_PATH + '/' + prefix
+
+        process_path(path, prefix, column, remove)
+
+
+def process_path(path: str, prefix: str, column: str, remove: bool):
+    print(f'Processing path {path} with prefix {prefix}:')
+
+    file = prefix_exists_in_metadata(prefix)
+    if file:
+        print(f'\tPrefix "{prefix}" is used in metadata file "{file}"')
+        return
+
+    print(f'\tPrefix "{prefix}" is NOT used in any metadata file')
+
+    if not additional_check_successed(column, path):
+        print('\t\tAdditional check for column-parameter not passed')
+        return
+
+    size = du(path)
+
+    print(f'\t\tSize of path {path}: {size}')
+
+    if remove:
+        print(f'\t\tTrying to remove path "{path}"...')
+
+        remove_data(path)
+        return
+
+    print(f'\t\tPath "{path}" is not removed because of remove-parameter is not specified')
+
+
+def prefix_exists_in_metadata(prefix: str):
+    for w in os.walk(CLICKHOUSE_PATH):
+        dirName = w[0]
+        filenames = w[2]
+
+        for file in filenames:
+            if not file.endswith('.sql'):
+                continue
+
+            with open(dirName + '/' + file) as f:
+                if f'\'{prefix}' in f.read():
+                    return file
+
+    return None
+
+
+def additional_check_successed(column: str, path: str):
+    if not column:
+        return False
+
+    for w in os.walk(path):
+        filenames = w[2]
+
+        columns = [file for file in filenames if column in file]
+        if columns:
+            return True
+
+    return False
+
+
+def du(path: str):
+    return subprocess.check_output(['du', '-sh', path]).split()[0].decode('utf-8')
+
+
+def remove_data(path: str):
+    def onerror(*args):
+        errors = [x for x in args]
+
+        print(f'\t\t\tERROR! {errors}')
+
+    shutil.rmtree(path=path, onerror=onerror)
