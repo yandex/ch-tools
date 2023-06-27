c616dfdea59b8bef141356e8749dce189ca5f047
myrrc <myrrc@yandex-team.ru>
2023-06-27 11:40:12 +0000

    fix
    X-Arc-Original-Commit: 9c387fa8919ccb6e5fe9182f7c1184ee3a28769e

--- src/chtools/monrun_checks/ch_log_errors.py
+++ src/chtools/monrun_checks/ch_log_errors.py
@@ -5,18 +5,8 @@ import re
 from chtools.common.result import Result
 
 
-default_regex = r'([0-9]{4}\.[0-9]{2}\.[0-9]{2}\ [0-9]{2}\:[0-9]{2}\:[0-9]{2})'
+regex = re.compile(r'^([0-9]{4}\.[0-9]{2}\.[0-9]{2}\ [0-9]{2}\:[0-9]{2}\:[0-9]{2})')
 default_exclude = r'e\.displayText\(\) = No message received'
-
-
-def validate_regex(ctx, param, value):
-    if not value:
-        return default_regex
-    try:
-        re.compile(value)
-    except re.error:
-        raise click.BadParameter('Value should be a valid regular expression.')
-    return value
 
 
 def validate_exclude(ctx, param, value):
@@ -33,12 +23,11 @@ def validate_exclude(ctx, param, value):
 @click.option('-c', '--critical', 'crit', type=int, default=60, help='Critical threshold.')
 @click.option('-w', '--warning', 'warn', type=int, default=6, help='Warning threshold.')
 @click.option('-n', '--watch-seconds', 'watch_seconds', type=int, default=600, help='Watch seconds.')
-@click.option('-r', '--regex', 'regex', default=default_regex, callback=validate_regex, help='Regular expresson.')
 @click.option('-e', '--exclude', 'exclude', default=default_exclude, callback=validate_exclude, help='Excluded error.')
 @click.option(
     '-f', '--logfile', 'logfile', default='/var/log/clickhouse-server/clickhouse-server.err.log', help='Log file path.'
 )
-def log_errors_command(crit, warn, watch_seconds, regex, exclude, logfile):
+def log_errors_command(crit, warn, watch_seconds, exclude, logfile):
     """
     Check errors in clickhouse log.
     """
