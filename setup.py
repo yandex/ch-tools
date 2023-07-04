8db087ef31d02038a3a4503972c6dfe194d8b494
Alexander Burmak <Alex-Burmak@yandex-team.ru>
2023-07-04 09:51:54 +0300

    Updated classifiers in setup.py

--- setup.py
+++ setup.py
@@ -30,6 +30,8 @@ setup(
         "Operating System :: POSIX :: BSD",
         "Operating System :: POSIX :: Linux",
         "Operating System :: Unix",
+        "Environment :: Console",
+        "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
