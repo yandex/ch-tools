201c18c9da1282baf119e10dd1d8b8844e468770
Yahor Shakh <88634916+yahor-s@users.noreply.github.com>
2024-01-25 16:34:49 +0300

    remove checks (#93)
    Co-authored-by: Yahor Shakh <egorshakh@yandex-team.ru>

--- ch_tools/monrun_checks/clickhouse_info.py
+++ ch_tools/monrun_checks/clickhouse_info.py
@@ -1,26 +1,9 @@
 import functools
 
-from ch_tools.monrun_checks.clickhouse_client import ClickhouseClient, ClickhousePort
+from ch_tools.monrun_checks.clickhouse_client import ClickhouseClient
 
 
 class ClickhouseInfo:
-    @classmethod
-    @functools.lru_cache(maxsize=1)
-    def get_versions_count(cls, ctx):
-        """
-        Count different clickhouse versions in cluster.
-        """
-        ch_client = ClickhouseClient(ctx)
-        #  I believe that all hosts in cluster have the same port set, so check current for security port
-        remote_command = (
-            "remoteSecure"
-            if ch_client.check_port(ClickhousePort.TCP_SECURE)
-            else "remote"
-        )
-        replicas = ",".join(cls.get_replicas(ctx))
-        query = f"""SELECT uniq(version()) FROM {remote_command}('{replicas}', system.one)"""
-        return int(ch_client.execute(query)[0][0])
-
     @classmethod
     @functools.lru_cache(maxsize=1)
     def get_replicas(cls, ctx):
