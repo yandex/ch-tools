7273d2e07f22b3d76f568efb60a2eb1389aee309
MikhailBurdukov <burdukvmikhail@gmail.com>
2024-04-05 07:39:17 +0000

    Fix tests

--- tests/modules/chadmin.py
+++ tests/modules/chadmin.py
@@ -8,10 +8,11 @@ class Chadmin:
     def exec_cmd(self, cmd):
         ch_admin_cmd = f"chadmin {cmd}"
         logging.debug("chadmin command:", ch_admin_cmd)
+
         result = self._container.exec_run(["bash", "-c", ch_admin_cmd], user="root")
         return result
 
-    def create_zk_node(self, zk_node, no_ch_config=True, recursive=True):
+    def create_zk_node(self, zk_node, no_ch_config=False, recursive=True):
         cmd = "zookeeper {use_config} create {make_parents} {node}".format(
             use_config="--no-ch-config" if no_ch_config else "",
             make_parents="--make-parents" if recursive else "",
