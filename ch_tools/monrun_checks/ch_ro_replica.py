dc5cbc67fba47d2f51a4553437e8704f0c7edda3
Konstantin Morozov <34001730+k-morozov@users.noreply.github.com>
2024-05-14 19:16:56 +0200

    additional info about RO tables (#179)
    * additional info about RO tables
    
    * fix ruff style
    
    * add replica_path for verbose info

--- ch_tools/monrun_checks/ch_ro_replica.py
+++ ch_tools/monrun_checks/ch_ro_replica.py
@@ -5,17 +5,54 @@ from ch_tools.common.result import CRIT, OK, Result
 
 
 @click.command("ro-replica")
+@click.option(
+    "-v",
+    "--verbose",
+    is_flag=True,
+    help="Show details about ro tables.",
+)
 @click.pass_context
-def ro_replica_command(ctx):
+def ro_replica_command(ctx, verbose=False):
     """
     Check for readonly replicated tables.
     """
-    query = "SELECT database, table FROM system.replicas WHERE is_readonly"
+    query = """
+        SELECT database, table, replica_path, last_queue_update_exception, zookeeper_exception
+        FROM system.replicas WHERE is_readonly
+    """
     response = clickhouse_client(ctx).query_json_data(query, compact=False)
     if response:
+        msg_verbose = ""
+
+        if verbose:
+            headers = [
+                "database",
+                "table",
+                "replica_path",
+                "last_queue_update_exception",
+                "zookeeper_exception",
+            ]
+
+            formatted_data = []
+
+            for item in response:
+                formatted_row = "\n".join(
+                    [
+                        f"{header}: {item[header]}"
+                        for header in headers
+                        if header in item
+                    ]
+                )
+                formatted_data.append(formatted_row)
+
+            msg_verbose = "\n\n".join(data for data in formatted_data)
+
         tables_str = ", ".join(
             f"{item['database']}.{item['table']}" for item in response
         )
-        return Result(CRIT, f"Readonly replica tables: {tables_str}")
+
+        return Result(
+            CRIT, f"Readonly replica tables: {tables_str}", verbose=msg_verbose
+        )
 
     return Result(OK)
