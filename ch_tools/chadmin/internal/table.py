9b951866d2364fc61b7f6e0f84f0f3a9a3123954
Konstantin Morozov <just.morozov.k@gmail.com>
2024-05-20 17:08:48 +0200

    up version

--- ch_tools/chadmin/internal/table.py
+++ ch_tools/chadmin/internal/table.py
@@ -382,7 +382,7 @@ def _remove_table_data_from_disk(ctx: Context, table_uuid: str, disk: str) -> No
 
     disk_config_path = make_ch_disks_config(disk)
 
-    if equal_ch_version(ctx, version="22.8") and not _is_should_use_ch_disk_remover(
+    if equal_ch_version(ctx, version="22.8.21.38") and not _is_should_use_ch_disk_remover(
         disk, table_data_path
     ):
         logging.info(
