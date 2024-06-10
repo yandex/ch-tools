c6e091e43cfd294d0b76d231f77dee79c752ec32
NikitaUnisikhin <nik.unisikhin.05@gmail.com>
2024-06-10 07:55:46 +0000

    Rewrite orphaned objects monrun check

--- ch_tools/monrun_checks/ch_orphaned_objects.py
+++ ch_tools/monrun_checks/ch_orphaned_objects.py
@@ -1,35 +1,10 @@
-from datetime import timedelta
-from typing import Optional
-
 import click
 
-from ch_tools.common.cli.parameters import TimeSpanParamType
-from ch_tools.common.clickhouse.config import get_clickhouse_config
-from ch_tools.common.commands.clean_object_storage import DEFAULT_GUARD_INTERVAL, clean
+from ch_tools.chadmin.cli.object_storage_group import ORPHANED_OBJECTS_LOG
 from ch_tools.common.result import CRIT, OK, WARNING, Result
-
-DRY_RUN = True
-ON_CLUSTER = True
-CLUSTER_NAME = "{cluster}"
-DISK_NAME = "object_storage"
-OBJECT_NAME_PREFIX = ""
 
 
 @click.command("orphaned-objects")
-@click.option(
-    "--keep-paths",
-    "keep_paths",
-    is_flag=True,
-    help=(
-        "Do not delete the collected paths from the local table, when the command completes."
-    ),
-)
-@click.option(
-    "--use-saved-list",
-    "use_saved_list",
-    is_flag=True,
-    help=("Use saved object list without traversing object storage again."),
-)
 @click.option(
     "-c",
     "--critical",
@@ -46,52 +21,17 @@ OBJECT_NAME_PREFIX = ""
     default=100 * 1024**2,
     help="Warning threshold.",
 )
-@click.option(
-    "--from-time",
-    "from_time",
-    default=None,
-    type=TimeSpanParamType(),
-    help=(
-        "Begin of inspecting interval in human-friendly format. "
-        "Objects with a modification time falling interval [now - from_time, now - to_time] are considered."
-    ),
-)
-@click.option(
-    "-g",
-    "--guard-interval",
-    "--to-time",
-    "to_time",
-    default=DEFAULT_GUARD_INTERVAL,
-    type=TimeSpanParamType(),
-    help=("End of inspecting interval in human-friendly format."),
-)
 @click.pass_context
 def orphaned_objects_command(
     ctx: click.Context,
-    keep_paths: bool,
-    use_saved_list: bool,
     crit: int,
     warn: int,
-    from_time: Optional[timedelta],
-    to_time: timedelta,
 ) -> Result:
-    ch_config = get_clickhouse_config(ctx)
-    ctx.obj[
-        "disk_configuration"
-    ] = ch_config.storage_configuration.s3_disk_configuaration(DISK_NAME)
-
-    _, total_size = clean(
-        ctx,
-        OBJECT_NAME_PREFIX,
-        from_time,
-        to_time,
-        ON_CLUSTER,
-        CLUSTER_NAME,
-        DRY_RUN,
-        keep_paths,
-        use_saved_list,
-    )
-
+    try:
+        with open(ORPHANED_OBJECTS_LOG, "r") as file:
+            total_size = int(file.read())
+    except FileNotFoundError:
+        total_size = 0
     msg = f"Total size: {total_size}"
     if total_size >= crit:
         return Result(CRIT, msg)
