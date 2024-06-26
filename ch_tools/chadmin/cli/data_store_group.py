278995a5a17125e646820c68e33e54542f947867
MikhailBurdukov <burdukvmikhail@gmail.com>
2024-06-26 14:52:55 +0000

    Review

--- ch_tools/chadmin/cli/data_store_group.py
+++ ch_tools/chadmin/cli/data_store_group.py
@@ -63,7 +63,7 @@ def clean_orphaned_tables_command(
             path_result = process_path(path, prefix, column, remove)
         except subprocess.CalledProcessError as e:
             if "No such file or directory" in e.stdout.decode("utf-8"):
-                print("Pass directory {} because it is removed: {}", path, e.stdout)
+                print("Spip directory {} because it is removed: {}", path, e.stdout)
                 continue
             raise
 
