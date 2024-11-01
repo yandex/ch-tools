d7ca9d3a5a8725777c484e6451d974215a41660b
Konstantin Morozov <34001730+k-morozov@users.noreply.github.com>
2024-11-01 17:12:53 +0100

    Detach broken partitions (#248)
    * init
    
    * fix style: import
    
    * fix style: black
    
    * apply comments
    
    * style: comma
    
    * A bit of refactoring
    
    * fix
    
    ---------
    
    Co-authored-by: MikhailBurdukov <burdukvmikhail@gmail.com>

--- ch_tools/chadmin/cli/data_store_group.py
+++ ch_tools/chadmin/cli/data_store_group.py
@@ -6,6 +6,7 @@ from typing import Dict, List, Optional, Tuple
 
 import boto3
 from click import Context, group, option, pass_context
+from cloup.constraints import AcceptAtMost, constraint
 
 from ch_tools.chadmin.cli.chadmin_group import Chadmin
 from ch_tools.chadmin.internal.clickhouse_disks import (
@@ -340,17 +341,34 @@ def collect_orphaned_sql_objects_recursive(
     default=False,
     help="Flag to reattach broken partitions.",
 )
+@option(
+    "--detach",
+    is_flag=True,
+    default=False,
+    help="Flag to detach broken partitions.",
+)
+@constraint(AcceptAtMost(1), ["detach", "reattach"])
 @pass_context
-def detect_broken_partitions(ctx, root_path, reattach):
+def detect_broken_partitions(ctx, root_path, reattach, detach):
     parts_paths_with_lost_keys = find_paths_to_part_with_lost_keys(ctx, root_path)
     partition_list = get_partitions_by_path(ctx, parts_paths_with_lost_keys)
 
     print_response(ctx, partition_list, default_format="table")
-    if reattach:
+
+    if reattach or detach:
         for partition_info in partition_list:
-            reattach_partition(
-                ctx, partition_info["table"], partition_info["partition"]
-            )
+            if reattach:
+                handle_partition(
+                    ctx, partition_info["table"], partition_info["partition"]
+                )
+            if detach:
+                handle_partition(
+                    ctx,
+                    partition_info["table"],
+                    partition_info["partition"],
+                    attach=False,
+                    detach=True,
+                )
 
 
 def find_paths_to_part_with_lost_keys(ctx: Context, root_path: str) -> List[str]:
@@ -466,30 +484,37 @@ def query_with_retry(ctx: Context, query: str, timeout: int, retries: int) -> bo
     return True
 
 
-def reattach_partition(ctx: Context, table: str, partition: str) -> bool:
+def handle_partition(
+    ctx: Context, table: str, partition: str, detach: bool = True, attach: bool = True
+) -> bool:
     """
-    Run Detach , Attach for given partition.
+    Handle the partition.
+    Run DETACH, ATTACH if needed.
     """
-    logging.debug(f"Going to reattach partition {partition} for table {table}")
-    detach_query = f"ALTER TABLE {table} DETACH PARTITION '{partition}'"
-    attach_query = f"ALTER TABLE {table} ATTACH PARTITION '{partition}'"
+    if not detach and not attach:
+        return False
 
-    res = query_with_retry(
-        ctx,
-        detach_query,
-        timeout=ATTACH_DETTACH_TIMEOUT,
-        retries=ATTACH_DETACH_QUERY_RETRY,
-    )
-    if not res:
-        return res
-
-    # To avoid keeping detached partitions, perform the attach query with double attempts.
-    res = query_with_retry(
-        ctx,
-        attach_query,
-        timeout=ATTACH_DETTACH_TIMEOUT,
-        retries=2 * ATTACH_DETACH_QUERY_RETRY,
-    )
-    if not res:
-        return res
+    if detach:
+        logging.debug(f"Going to attach partition {partition} for table {table}")
+        detach_query = f"ALTER TABLE {table} DETACH PARTITION '{partition}'"
+        res = query_with_retry(
+            ctx,
+            detach_query,
+            timeout=ATTACH_DETTACH_TIMEOUT,
+            retries=ATTACH_DETACH_QUERY_RETRY,
+        )
+        if not res:
+            return res
+    if attach:
+        logging.debug(f"Going to detach partition {partition} for table {table}")
+        attach_query = f"ALTER TABLE {table} ATTACH PARTITION '{partition}'"
+        # To avoid keeping detached partitions, perform the attach query with double attempts.
+        res = query_with_retry(
+            ctx,
+            attach_query,
+            timeout=ATTACH_DETTACH_TIMEOUT,
+            retries=2 * ATTACH_DETACH_QUERY_RETRY,
+        )
+        if not res:
+            return res
     return True
