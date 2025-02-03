91300592de85829df99e9f789e7db9c15e97497f
Nikita Goncharov <28632856+goncharovnikita@users.noreply.github.com>
2025-02-03 10:38:42 +0300

    ch-monitoring backup: ignore backups in status 'creating'

--- tests/unit/monrun/test_backup.py
+++ tests/unit/monrun/test_backup.py
@@ -1,8 +1,13 @@
-from typing import Dict, Sequence
+from datetime import datetime, timedelta, timezone
+from typing import Dict, List, Sequence
 
 from pytest import mark
 
-from ch_tools.monrun_checks.ch_backup import _is_backup_failed_by_userfault_error
+from ch_tools.common.result import CRIT, OK, WARNING
+from ch_tools.monrun_checks.ch_backup import (
+    _check_backup_age,
+    _is_backup_failed_by_userfault_error,
+)
 
 
 @mark.parametrize(
@@ -67,3 +72,118 @@ def test_is_backup_failed_by_userfault_error(
     backups: Sequence[Dict], result: bool
 ) -> None:
     assert _is_backup_failed_by_userfault_error(backups) == result
+
+
+@mark.parametrize(
+    ["backups", "expected_code", "expected_message"],
+    [
+        (
+            [],
+            OK,
+            "OK",
+        ),
+        (
+            [
+                {
+                    "state": "created",
+                    "start_time": datetime.now(timezone.utc).strftime(
+                        "%Y-%m-%d %H:%M:%S %z"
+                    ),
+                }
+            ],
+            OK,
+            "OK",
+        ),
+        (
+            [
+                {
+                    "state": "created",
+                    "start_time": (
+                        datetime.now(timezone.utc) - timedelta(days=1)
+                    ).strftime("%Y-%m-%d %H:%M:%S %z"),
+                }
+            ],
+            OK,
+            "OK",
+        ),
+        (
+            [
+                {
+                    "state": "created",
+                    "start_time": (
+                        datetime.now(timezone.utc) - timedelta(days=3)
+                    ).strftime("%Y-%m-%d %H:%M:%S %z"),
+                }
+            ],
+            WARNING,
+            "Last backup was created 3 days ago",
+        ),
+        (
+            [
+                {
+                    "state": "created",
+                    "start_time": (
+                        datetime.now(timezone.utc) - timedelta(days=5)
+                    ).strftime("%Y-%m-%d %H:%M:%S %z"),
+                }
+            ],
+            CRIT,
+            "Last backup was created 5 days ago",
+        ),
+        (
+            [
+                {
+                    "state": "creating",
+                    "start_time": datetime.now(timezone.utc).strftime(
+                        "%Y-%m-%d %H:%M:%S %z"
+                    ),
+                },
+                {
+                    "state": "failed",
+                    "start_time": (
+                        datetime.now(timezone.utc) - timedelta(days=3)
+                    ).strftime("%Y-%m-%d %H:%M:%S %z"),
+                },
+            ],
+            OK,
+            "OK",
+        ),
+        (
+            [
+                {
+                    "state": "creating",
+                    "start_time": datetime.now(timezone.utc).strftime(
+                        "%Y-%m-%d %H:%M:%S %z"
+                    ),
+                },
+                {
+                    "state": "failed",
+                    "start_time": (
+                        datetime.now(timezone.utc) - timedelta(days=3)
+                    ).strftime("%Y-%m-%d %H:%M:%S %z"),
+                },
+                {
+                    "state": "created",
+                    "start_time": (
+                        datetime.now(timezone.utc) - timedelta(days=5)
+                    ).strftime("%Y-%m-%d %H:%M:%S %z"),
+                },
+            ],
+            CRIT,
+            "Last backup was created 5 days ago",
+        ),
+    ],
+)
+def test_check_backup_age(
+    backups: List[Dict],
+    expected_code: int,
+    expected_message: str,
+) -> None:
+    check_result = _check_backup_age(
+        backups,
+        timedelta(days=3),
+        timedelta(days=5),
+    )
+
+    assert check_result.code == expected_code
+    assert check_result.message == expected_message
