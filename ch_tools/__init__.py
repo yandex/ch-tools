4ff03d555097d6d96e7551ae5f1fad826d7d0053
Konstantin Morozov <just.morozov.k@gmail.com>
2025-05-27 11:26:52 +0200

    add warning stub

--- ch_tools/__init__.py
+++ ch_tools/__init__.py
@@ -1,5 +1,12 @@
 """A set of tools for administration and diagnostics of ClickHouse DBMS."""
 
+import warnings
+
 from pkg_resources import resource_string
+
+warnings.filterwarnings(
+    "ignore", category=UserWarning, message="pkg_resources is deprecated"
+)
+
 
 __version__ = resource_string(__name__, "version.txt").decode().strip()
