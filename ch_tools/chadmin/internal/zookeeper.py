5e10aed125c4dbe66892ffce543fbb65419bfc87
Konstantin Morozov <just.morozov.k@gmail.com>
2025-06-23 20:28:44 +0200

    up

--- ch_tools/chadmin/internal/zookeeper.py
+++ ch_tools/chadmin/internal/zookeeper.py
@@ -3,12 +3,9 @@ import re
 from collections import deque
 from contextlib import contextmanager
 from math import sqrt
-from typing import Tuple
 
-from click import Context
 from kazoo.client import KazooClient
 from kazoo.exceptions import NoNodeError, NotEmptyError
-from kazoo.protocol.states import ZnodeStat
 
 from ch_tools.chadmin.internal.utils import chunked, replace_macros
 from ch_tools.common import logging
@@ -25,15 +22,6 @@ def get_zk_node(ctx, path, binary=False):
         path = format_path(ctx, path)
         value = zk.get(path)[0]
         return value if binary else value.decode().strip()
-
-
-def get_zk_node_with_version(
-    ctx: Context, path: str, binary: bool = False
-) -> Tuple[str, ZnodeStat]:
-    with zk_client(ctx) as zk:
-        path = format_path(ctx, path)
-        value, version = zk.get(path)
-        return value if binary else value.decode().strip(), version
 
 
 def check_zk_node(ctx, path):
