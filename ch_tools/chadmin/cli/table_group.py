b7386833df4b00ecc9b654660dde768bafe72505
Konstantin Morozov <just.morozov.k@gmail.com>
2025-07-15 21:17:06 +0200

    up

--- ch_tools/chadmin/cli/table_group.py
+++ ch_tools/chadmin/cli/table_group.py
@@ -923,7 +923,7 @@ def change_uuid_command(
                 raise RuntimeError(
                     f"Table {table_name} is not replicated. Failed get uuid from table_shared_id node."
                 )
-            
+
             assert metadata.replica_path is not None
 
             replica_path = metadata.replica_path
