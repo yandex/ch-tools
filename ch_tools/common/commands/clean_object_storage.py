1232e078f7894fa16f8c1ea763a13735277d1c55
MedvedewEM <egor-medvedev@yandex-team.ru>
2025-09-13 11:28:44 +0100

    Skip sanity-check for orphaned objects' cleaning in dry-run mode (#390)
    * Skip sanity-check for orphaned objects' cleaning in dry-run mode
    
    * move check before any work
    
    ---------
    
    Co-authored-by: MikhailBurdukov <burdukvmikhail@gmail.com>

--- ch_tools/common/commands/clean_object_storage.py
+++ ch_tools/common/commands/clean_object_storage.py
@@ -170,6 +170,7 @@ def _sanity_check_before_cleanup(
     remote_data_paths_table: str,
     verify_paths_regex: Optional[str],
     clean_scope: CleanScope,
+    dry_run: bool,
 ) -> None:
 
     size_error_rate_threshold_fraction = ctx.obj["config"]["object_storage"]["clean"][
@@ -187,6 +188,12 @@ def _sanity_check_before_cleanup(
         if match_ch_version(ctx, min_version="24.3")
         else {}
     )
+
+    def raise_or_warn(dry_run: bool, msg: str) -> None:
+        if not dry_run:
+            raise RuntimeError(msg)
+
+        logging.warning(f"Warning: {msg}")
 
     def perform_check_paths() -> None:
         ### Compare path from system.remote_data_path and from list to delete. They must match the regex.
@@ -214,35 +221,43 @@ def _sanity_check_before_cleanup(
         )[0]
 
         if not re.match(paths_regex, path_form_ch):
-            raise RuntimeError(
+            raise_or_warn(
+                dry_run,
                 "Sanity check not passed, because remote_path({}) doesn't matches the regex({}).".format(
                     path_form_ch, paths_regex
-                )
+                ),
             )
 
         for orphaned_object in orphaned_objects_iterator():
             if not re.match(paths_regex, orphaned_object.path):
-                raise RuntimeError(
+                raise_or_warn(
+                    dry_run,
                     "Sanity check not passed, because orphaned object({}) doesn't matches the regex({}).".format(
                         orphaned_object.path, paths_regex
-                    )
+                    ),
                 )
 
     def perform_check_size() -> None:
+
+        if listing_size_in_bucket == 0:
+            return
+
         ### Total size of objects after cleanup must be very close to sum(bytes) FROM system.remote_data_paths
         size_to_delete = 0
         for orphaned_object in orphaned_objects_iterator():
             size_to_delete += orphaned_object.size
+
         if (
             listing_size_in_bucket * size_error_rate_threshold_fraction
             <= size_to_delete
         ):
-            raise RuntimeError(
+            raise_or_warn(
+                dry_run,
                 "Potentially dangerous operation: Going to remove more than {}% of bucket content. To remove {}; listing size {}".format(
                     int(size_error_rate_threshold_fraction * 100),
                     format_size(listing_size_in_bucket),
                     format_size(size_to_delete),
-                )
+                ),
             )
 
     perform_check_paths()
@@ -355,6 +370,7 @@ def _clean_object_storage(
             remote_data_paths_table,
             verify_paths_regex,
             clean_scope,
+            dry_run,
         )
 
     deleted, total_size = 0, 0
