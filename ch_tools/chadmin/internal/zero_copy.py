e4311148c6d21c035e241de1ae905e3abb156c4e
kirillgarbar <sexyboar322@gmail.com>
2025-12-10 15:26:37 +0300

    Copy values of old zero-copy locks

--- ch_tools/chadmin/internal/zero_copy.py
+++ ch_tools/chadmin/internal/zero_copy.py
@@ -34,6 +34,8 @@ def generate_zero_copy_lock_tasks(
     zk: KazooClient,
     dry_run: bool = False,
     zero_copy_path: Optional[str] = None,
+    zero_copy_path_old: Optional[str] = None,
+    copy_values: bool = False,
 ) -> Generator[WorkerTask, None, None]:
     """Generate tasks for creating zero-copy locks for given table/replica."""
     storage_config = S3DiskConfiguration.from_config(
@@ -42,8 +44,13 @@ def generate_zero_copy_lock_tasks(
         ctx.obj["config"]["object_storage"]["bucket_name_prefix"],
     )
 
-    if zero_copy_path is None:
-        zero_copy_path = _get_zero_copy_zookeeper_path(ctx, "s3", table["uuid"])
+    zero_copy_path = _get_zero_copy_zookeeper_path_for_disk_type(
+        ctx, storage_config.OBJECT_STORAGE_TYPE, zero_copy_path, table["uuid"]
+    )
+    zero_copy_path_old = _get_zero_copy_zookeeper_path_for_disk_type(
+        ctx, storage_config.OBJECT_STORAGE_TYPE, zero_copy_path_old, table["uuid"]
+    )
+    should_copy_lock_values = copy_values and zero_copy_path != zero_copy_path_old
 
     part_info = list_parts(
         ctx,
@@ -55,13 +62,15 @@ def generate_zero_copy_lock_tasks(
     )
 
     for part in part_info:
-        lock_path = _get_zero_copy_lock_path(
+        lock_path, lock_path_old = _get_zero_copy_lock_path(
             storage_config.prefix,
             zero_copy_path,
+            zero_copy_path_old,
             table["uuid"],
             part,
             replica,
         )
+
         part_path = _get_part_path_in_zk(part["name"], zookeeper_path, replica)
 
         task_id = f"{table['database']}.{table['name']}.{replica}.{part['name']}"
@@ -71,8 +80,10 @@ def generate_zero_copy_lock_tasks(
             kwargs={
                 "zk": zk,
                 "lock_path": lock_path,
+                "lock_path_old": lock_path_old,
                 "part_path": part_path,
                 "dry_run": dry_run,
+                "copy_values": should_copy_lock_values,
             },
         )
 
@@ -94,15 +105,18 @@ def _get_first_checksums_blob_path(object_storage_prefix: str, part: dict) -> st
 def _get_zero_copy_lock_path(
     object_storage_prefix: str,
     zero_copy_path: str,
+    zero_copy_path_old: str,
     table_uuid: str,
     part: dict,
     replica: str,
-) -> str:
+) -> tuple[str, str]:
     blob_path = _get_first_checksums_blob_path(object_storage_prefix, part)
     object_storage_path = blob_path.replace("/", "_")
 
     return os.path.join(
         zero_copy_path, table_uuid, part["name"], object_storage_path, replica
+    ), os.path.join(
+        zero_copy_path_old, table_uuid, part["name"], object_storage_path, replica
     )
 
 
@@ -114,8 +128,11 @@ def _get_part_path_in_zk(
     return os.path.join(zookeeper_path, "replicas", replica, "parts", part)
 
 
-def _get_zero_copy_zookeeper_path(
-    ctx: Context, disk_type: Optional[str] = "s3", table_uuid: Optional[str] = None
+def _get_zero_copy_zookeeper_path_for_disk_type(
+    ctx: Context,
+    disk_type: Optional[str] = "s3",
+    zero_copy_path: Optional[str] = None,
+    table_uuid: Optional[str] = None,
 ) -> str:
     """
     Returns ZooKeeper path for zero-copy table-independent info.
@@ -123,6 +140,9 @@ def _get_zero_copy_zookeeper_path(
     '/clickhouse/zero_copy/zero_copy_s3' is default for s3 disk.
     """
     disk_dir = f"zero_copy_{disk_type}"
+
+    if zero_copy_path:
+        return os.path.join(zero_copy_path, disk_dir)
 
     if table_uuid:
         get_settings_query = (
@@ -149,8 +169,10 @@ def _get_zero_copy_zookeeper_path(
 def _create_single_zero_copy_lock(
     zk: KazooClient,
     lock_path: str,
+    lock_path_old: str,
     part_path: str,
     dry_run: bool = False,
+    copy_values: bool = False,
 ) -> None:
     """Create a single zero-copy lock."""
 
@@ -159,15 +181,22 @@ def _create_single_zero_copy_lock(
         exception_types=(NoNodeError, NodeExistsError), max_attempts=3, max_interval=1
     )
     def _create_lock_in_transaction(
-        zk: KazooClient, lock_path: str, part_path: str, part_node_version: int
+        zk: KazooClient,
+        lock_path: str,
+        value: bytes,
+        part_path: str,
+        part_node_version: int,
     ) -> None:
         parents_to_create = _get_parent_paths_to_create(zk, lock_path)
 
         create_transaction = zk.transaction()
         create_transaction.check(part_path, part_node_version)
 
-        for parent in parents_to_create:
-            create_transaction.create(parent)
+        for i, parent in enumerate(parents_to_create):
+            if len(parents_to_create) >= 2 and i == len(parents_to_create) - 2:
+                create_transaction.create(parent, value)
+            else:
+                create_transaction.create(parent)
 
         create_transaction.create(lock_path)
         results = create_transaction.commit()
@@ -187,11 +216,23 @@ def _create_single_zero_copy_lock(
         return
 
     logging.info("Creating zero-copy lock at {}", lock_path)
+
+    value = b""
+    if copy_values and zk.exists(lock_path_old):
+        # Value is stored in 'part' node, full path is '.../part/blob_path/replica'
+        value_path = os.path.dirname(os.path.dirname(lock_path_old))
+        try:
+            value, _ = zk.get(value_path)
+            logging.debug("Found old lock at {}, will copy it's value", lock_path_old)
+        except Exception:
+            # Lock was concurrently removed
+            pass
+
     if dry_run:
         return
 
     try:
-        _create_lock_in_transaction(zk, lock_path, part_path, part_node.version)
+        _create_lock_in_transaction(zk, lock_path, value, part_path, part_node.version)
     except Exception as e:
         raise RuntimeError(
             f"Failed to create zero-copy lock at {lock_path}, reason: {e}"
