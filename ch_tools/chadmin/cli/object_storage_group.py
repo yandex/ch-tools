b2f96d7c174738d1593c7a8c37a6d59eb52dfb59
kirillgarbar <sexyboar322@gmail.com>
2025-12-15 15:35:50 +0300

    Move deduplication code to separate module

--- ch_tools/chadmin/cli/object_storage_group.py
+++ ch_tools/chadmin/cli/object_storage_group.py
@@ -1,4 +1,3 @@
-import os
 from datetime import timedelta
 from typing import Any, Optional
 
@@ -16,7 +15,7 @@ from cloup import option_group
 from humanfriendly import format_size
 
 from ch_tools.chadmin.cli.chadmin_group import Chadmin
-from ch_tools.chadmin.cli.partition_group import get_partitions
+from ch_tools.chadmin.internal.deduplicate import deduplicate
 from ch_tools.chadmin.internal.object_storage.orphaned_objects_state import (
     OrphanedObjectsState,
 )
@@ -24,24 +23,12 @@ from ch_tools.chadmin.internal.object_storage.s3_cleanup_stats import (
     ResultStat,
     StatisticsPeriod,
 )
-from ch_tools.chadmin.internal.partition import attach_partition, detach_partition
 from ch_tools.chadmin.internal.system import match_ch_version
-from ch_tools.chadmin.internal.table import (
-    has_data_on_disk,
-    list_tables,
-    set_table_setting,
-)
-from ch_tools.chadmin.internal.table_info import TableInfo
-from ch_tools.chadmin.internal.utils import execute_query_on_shard
 from ch_tools.chadmin.internal.zookeeper import (
     check_zk_node,
     create_zk_nodes,
-    delete_zk_node,
-    list_children,
     update_zk_nodes,
-    zk_client,
 )
-from ch_tools.common import logging
 from ch_tools.common.cli.formatting import format_bytes, print_response
 from ch_tools.common.cli.parameters import TimeSpanParamType
 from ch_tools.common.clickhouse.config import get_clickhouse_config
@@ -53,17 +40,11 @@ from ch_tools.common.commands.object_storage import (
     collect_object_storage_info,
     get_object_storage_space_usage,
 )
-from ch_tools.common.process_pool import WorkerTask, execute_tasks_in_parallel
 
 # Use big enough timeout for stream HTTP query
 STREAM_TIMEOUT = 10 * 60
 STATE_LOCAL_PATH = "/tmp/object_storage_cleanup_state.json"
 DEFAULT_CLUSTER_NAME = "{cluster}"
-ALWAYS_FETCH_ON_ATTACH_SETTING = (
-    "max_suspicious_broken_parts"  # "always_fetch_instead_of_attach_zero_copy"
-)
-DEDUPLICATION_ROOT_PATH = "deduplication"
-DEDUPLICATED_TABLES_PATH = os.path.join(DEDUPLICATION_ROOT_PATH, "done")
 
 
 @group("object-storage", cls=Chadmin)
@@ -247,7 +228,9 @@ def clean_command(
 
         if store_state_zk_path:
             _store_state_zk_save(
-                ctx, store_state_zk_path, state.to_json().encode("utf-8")
+                ctx,
+                store_state_zk_path,
+                state,
             )
 
         if store_state_local:
@@ -435,154 +418,20 @@ def deduplicate_command(
     Requires custom ClickHouse build with copying of part's data on attach instead of detach.
     In case of errors saves state in ZK to use it on retry.
     """
-    disk: S3DiskConfiguration = ctx.obj["disk_configuration"]
-    deduplicated_tables = (
-        set()
-        if ignore_saved_state
-        else set(list_children(ctx, DEDUPLICATED_TABLES_PATH))
+    deduplicate(
+        ctx=ctx,
+        database=database,
+        table=table,
+        partition_id=partition_id,
+        min_partition_id=min_partition_id,
+        max_partition_id=max_partition_id,
+        max_workers=max_workers,
+        ignore_saved_state=ignore_saved_state,
+        dry_run=dry_run,
     )
 
-    tables = [
-        table
-        for table in list_tables(
-            ctx,
-            database_name=database,
-            table_name=table,
-            engine_pattern="Replicated%MergeTree",
-        )
-        if has_data_on_disk(ctx, table, disk.name)
-        and f"{table['database']}.{table['name']}" not in deduplicated_tables
-    ]
 
-    if not dry_run:
-        create_zk_nodes(
-            ctx, [DEDUPLICATED_TABLES_PATH], make_parents=True, exists_ok=True
-        )
-
-    if deduplicated_tables:
-        logging.info("Already deduplicated tables {}", deduplicated_tables)
-    logging.info(
-        "Will deduplicate zero-copy data for tables {}",
-        [f"{table['database']}.{table['name']}" for table in tables],
-    )
-
-    tasks = _get_deduplication_tasks(
-        ctx,
-        disk,
-        tables,
-        partition_id,
-        min_partition_id,
-        max_partition_id,
-        ignore_saved_state,
-        dry_run,
-    )
-
-    # Load zk client to context to share it in threads
-    with zk_client(ctx):
-        execute_tasks_in_parallel(tasks, max_workers, keep_going=False)
-
-    delete_zk_node(ctx, DEDUPLICATION_ROOT_PATH, dry_run=dry_run)
-
-
-def _get_deduplication_tasks(
-    ctx: Context,
-    disk: S3DiskConfiguration,
-    tables: list[TableInfo],
-    partition_id: Optional[str],
-    min_partition_id: Optional[str],
-    max_partition_id: Optional[str],
-    ignore_saved_state: bool,
-    dry_run: bool,
-) -> list[WorkerTask]:
-    def _task(table_info: TableInfo) -> None:
-        set_table_setting(
-            ctx, table_info, ALWAYS_FETCH_ON_ATTACH_SETTING, 1, dry_run=dry_run
-        )
-
-        min_partition_id_actual = (
-            min_partition_id
-            if ignore_saved_state
-            else _get_min_partition_to_deduplicate(ctx, table_info, min_partition_id)
-        )
-
-        partitions = get_partitions(
-            ctx,
-            table_info["database"],
-            table_info["name"],
-            partition_id=partition_id,
-            min_partition_id=min_partition_id_actual,
-            max_partition_id=max_partition_id,
-            disk_name=disk.name,
-            format_="JSON",
-        )["data"]
-
-        for p in partitions:
-            try:
-                detach_partition(
-                    ctx, p["database"], p["table"], p["partition_id"], dry_run=dry_run
-                )
-                attach_partition(
-                    ctx, p["database"], p["table"], p["partition_id"], dry_run=dry_run
-                )
-            except Exception:
-                if not dry_run:
-                    _store_state_zk_save(
-                        ctx,
-                        _get_table_deduplication_zk_path(table_info),
-                        p["name"],
-                    )
-
-        execute_query_on_shard(
-            ctx,
-            f"SYSTEM SYNC REPLICA {table_info['database']}.{table_info['name']}",
-            format_=None,
-            dry_run=dry_run,
-        )
-        set_table_setting(
-            ctx, table_info, ALWAYS_FETCH_ON_ATTACH_SETTING, dry_run=dry_run
-        )
-
-        if not dry_run:
-            create_zk_nodes(
-                ctx,
-                [
-                    os.path.join(
-                        DEDUPLICATED_TABLES_PATH,
-                        f"{table_info['database']}.{table_info['name']}",
-                    )
-                ],
-            )
-
-    return [
-        WorkerTask(f"{table['database']}.{table['name']}", _task, {"table_info": table})
-        for table in tables
-    ]
-
-
-def _get_min_partition_to_deduplicate(
-    ctx: Context, table: TableInfo, min_partition_id: Optional[str]
-) -> Optional[str]:
-    min_partition_from_zk = _get_last_reattached_partition_from_zk(ctx, table)
-    if min_partition_id or min_partition_from_zk:
-        if min_partition_id and min_partition_from_zk:
-            return min(min_partition_id, min_partition_from_zk)
-        return min_partition_from_zk if min_partition_from_zk else min_partition_id
-    return None
-
-
-def _get_last_reattached_partition_from_zk(
-    ctx: Context, table: TableInfo
-) -> Optional[str]:
-    path = _get_table_deduplication_zk_path(table)
-    partitions = list_children(ctx, path)
-    return min(partitions) if partitions else None
-
-
-def _get_table_deduplication_zk_path(table: TableInfo) -> str:
-    return os.path.join(DEDUPLICATION_ROOT_PATH, table["database"], table["name"])
-
-
-def _store_state_zk_save(ctx: Context, path: str, state: Any) -> None:
+def _store_state_zk_save(ctx: Context, path: str, state: OrphanedObjectsState) -> None:
     if not check_zk_node(ctx, path):
         create_zk_nodes(ctx, [path], make_parents=True)
     state_data = state.to_json().encode("utf-8")
---
+++ ch_tools/chadmin/internal/deduplicate.py
@@ -0,0 +1,189 @@
+import os
+from typing import Optional
+from click import Context
+
+from ch_tools.chadmin.cli.partition_group import get_partitions
+from ch_tools.chadmin.internal.partition import attach_partition, detach_partition
+from ch_tools.chadmin.internal.table import (
+    has_data_on_disk,
+    list_tables,
+    set_table_setting,
+)
+from ch_tools.chadmin.internal.table_info import TableInfo
+from ch_tools.chadmin.internal.utils import execute_query_on_shard
+from ch_tools.chadmin.internal.zookeeper import (
+    create_zk_nodes,
+    delete_zk_node,
+    list_children,
+    update_zk_nodes,
+    zk_client,
+)
+from ch_tools.common import logging
+from ch_tools.common.clickhouse.config.storage_configuration import S3DiskConfiguration
+from ch_tools.common.process_pool import WorkerTask, execute_tasks_in_parallel
+
+ALWAYS_FETCH_ON_ATTACH_SETTING = (
+    "max_suspicious_broken_parts"  # "always_fetch_instead_of_attach_zero_copy"
+)
+DEDUPLICATION_ROOT_PATH = "deduplication"
+DEDUPLICATED_TABLES_PATH = os.path.join(DEDUPLICATION_ROOT_PATH, "done")
+
+
+def deduplicate(
+    ctx: Context,
+    database: Optional[str],
+    table: Optional[str],
+    partition_id: Optional[str],
+    min_partition_id: Optional[str],
+    max_partition_id: Optional[str],
+    max_workers: int,
+    ignore_saved_state: bool,
+    dry_run: bool,
+) -> None:
+    disk: S3DiskConfiguration = ctx.obj["disk_configuration"]
+    deduplicated_tables = (
+        set()
+        if ignore_saved_state
+        else set(list_children(ctx, DEDUPLICATED_TABLES_PATH))
+    )
+
+    tables = [
+        table
+        for table in list_tables(
+            ctx,
+            database_name=database,
+            table_name=table,
+            engine_pattern="Replicated%MergeTree",
+        )
+        if has_data_on_disk(ctx, table, disk.name)
+        and f"{table['database']}.{table['name']}" not in deduplicated_tables
+    ]
+
+    if not dry_run:
+        create_zk_nodes(
+            ctx, [DEDUPLICATED_TABLES_PATH], make_parents=True, exists_ok=True
+        )
+
+    if deduplicated_tables:
+        logging.info("Already deduplicated tables {}", deduplicated_tables)
+    logging.info(
+        "Will deduplicate zero-copy data for tables {}",
+        [f"{table['database']}.{table['name']}" for table in tables],
+    )
+
+    tasks = _get_deduplication_tasks(
+        ctx,
+        disk,
+        tables,
+        partition_id,
+        min_partition_id,
+        max_partition_id,
+        ignore_saved_state,
+        dry_run,
+    )
+
+    # Load zk client to context to share it in threads
+    with zk_client(ctx):
+        execute_tasks_in_parallel(tasks, max_workers, keep_going=False)
+
+    delete_zk_node(ctx, DEDUPLICATION_ROOT_PATH, dry_run=dry_run)
+
+
+def _get_deduplication_tasks(
+    ctx: Context,
+    disk: S3DiskConfiguration,
+    tables: list[TableInfo],
+    partition_id: Optional[str],
+    min_partition_id: Optional[str],
+    max_partition_id: Optional[str],
+    ignore_saved_state: bool,
+    dry_run: bool,
+) -> list[WorkerTask]:
+    def _task(table_info: TableInfo) -> None:
+        set_table_setting(
+            ctx, table_info, ALWAYS_FETCH_ON_ATTACH_SETTING, 1, dry_run=dry_run
+        )
+
+        min_partition_id_actual = (
+            min_partition_id
+            if ignore_saved_state
+            else _get_min_partition_to_deduplicate(ctx, table_info, min_partition_id)
+        )
+
+        # TODO: delete old partition node
+
+        partitions = get_partitions(
+            ctx,
+            table_info["database"],
+            table_info["name"],
+            partition_id=partition_id,
+            min_partition_id=min_partition_id_actual,
+            max_partition_id=max_partition_id,
+            disk_name=disk.name,
+            format_="JSON",
+        )["data"]
+
+        for p in partitions:
+            try:
+                detach_partition(
+                    ctx, p["database"], p["table"], p["partition_id"], dry_run=dry_run
+                )
+                attach_partition(
+                    ctx, p["database"], p["table"], p["partition_id"], dry_run=dry_run
+                )
+            except Exception:
+                if not dry_run:
+                    update_zk_nodes(
+                        ctx,
+                        [_get_table_deduplication_zk_path(table_info)],
+                        p["name"],
+                    )
+
+        execute_query_on_shard(
+            ctx,
+            f"SYSTEM SYNC REPLICA {table_info['database']}.{table_info['name']}",
+            format_=None,
+            dry_run=dry_run,
+        )
+        set_table_setting(
+            ctx, table_info, ALWAYS_FETCH_ON_ATTACH_SETTING, dry_run=dry_run
+        )
+
+        if not dry_run:
+            create_zk_nodes(
+                ctx,
+                [
+                    os.path.join(
+                        DEDUPLICATED_TABLES_PATH,
+                        f"{table_info['database']}.{table_info['name']}",
+                    )
+                ],
+            )
+
+    return [
+        WorkerTask(f"{table['database']}.{table['name']}", _task, {"table_info": table})
+        for table in tables
+    ]
+
+
+def _get_min_partition_to_deduplicate(
+    ctx: Context, table: TableInfo, min_partition_id: Optional[str]
+) -> Optional[str]:
+    min_partition_from_zk = _get_last_reattached_partition_from_zk(ctx, table)
+    if min_partition_id or min_partition_from_zk:
+        if min_partition_id and min_partition_from_zk:
+            return min(min_partition_id, min_partition_from_zk)
+        return min_partition_from_zk if min_partition_from_zk else min_partition_id
+    return None
+
+
+def _get_last_reattached_partition_from_zk(
+    ctx: Context, table: TableInfo
+) -> Optional[str]:
+    path = _get_table_deduplication_zk_path(table)
+    partitions = list_children(ctx, path)
+    return min(partitions) if partitions else None
+
+
+def _get_table_deduplication_zk_path(table: TableInfo) -> str:
+    return os.path.join(DEDUPLICATION_ROOT_PATH, table["database"], table["name"])
