aae75c4ada77f70cf54c6e0f53f62550948855ad
Alexander Burmak <Alex-Burmak@yandex-team.ru>
2026-02-22 13:30:48 +0300

    --database option for "ch-moniroing ro-replica" command

--- ch_tools/monrun_checks/ch_ro_replica.py
+++ ch_tools/monrun_checks/ch_ro_replica.py
@@ -11,48 +11,60 @@ from ch_tools.common.result import CRIT, OK, Result
     is_flag=True,
     help="Show details about ro tables.",
 )
+@click.option(
+    "-d",
+    "--database",
+    "database_pattern",
+    help="Check tables only in the databases that matches specified pattern.",
+)
 @click.pass_context
-def ro_replica_command(ctx: click.Context, verbose: bool = False) -> Result:
+def ro_replica_command(
+    ctx: click.Context,
+    verbose: bool,
+    database_pattern: str,
+) -> Result:
     """
     Check for readonly replicated tables.
     """
     query = """
         SELECT database, table, replica_path, last_queue_update_exception, zookeeper_exception
-        FROM system.replicas WHERE is_readonly
+        FROM system.replicas
+        WHERE is_readonly
+        {% if database_pattern -%}
+          AND database {{ format_str_match(database_pattern) }}
+        {% endif -%}
     """
-    response = clickhouse_client(ctx).query_json_data(query, compact=False)
-    if response:
-        msg_verbose = ""
+    query_result = clickhouse_client(ctx).query_json_data(
+        query=query,
+        query_args={
+            "database_pattern": database_pattern,
+        },
+        compact=False,
+    )
+    if not query_result:
+        return Result(OK)
 
-        if verbose:
-            headers = [
-                "database",
-                "table",
-                "replica_path",
-                "last_queue_update_exception",
-                "zookeeper_exception",
-            ]
+    verbose_msg = ""
+    if verbose:
+        headers = [
+            "database",
+            "table",
+            "replica_path",
+            "last_queue_update_exception",
+            "zookeeper_exception",
+        ]
 
-            formatted_data = []
+        formatted_data = []
+        for item in query_result:
+            formatted_row = "\n".join(
+                [f"{header}: {item[header]}" for header in headers if header in item]
+            )
+            formatted_data.append(formatted_row)
 
-            for item in response:
-                formatted_row = "\n".join(
-                    [
-                        f"{header}: {item[header]}"
-                        for header in headers
-                        if header in item
-                    ]
-                )
-                formatted_data.append(formatted_row)
+        verbose_msg = "\n\n".join(data for data in formatted_data)
 
-            msg_verbose = "\n\n".join(data for data in formatted_data)
+    tables_str = ", ".join(
+        f"{item['database']}.{item['table']}" for item in query_result
+    )
 
-        tables_str = ", ".join(
-            f"{item['database']}.{item['table']}" for item in response
-        )
-
-        return Result(
-            CRIT, f"Readonly replica tables: {tables_str}", verbose=msg_verbose
-        )
-
-    return Result(OK)
+    return Result(CRIT, f"Readonly replica tables: {tables_str}", verbose=verbose_msg)
