4ca29840569c7c087540278152c60aa3dff22760
Alexander Burmak <Alex-Burmak@yandex-team.ru>
2026-06-01 18:56:56 +0300

    Code cleanup: remove deprecated options and dependency on salt stack from "ch-monitoring resetup-state" command

--- ch_tools/monrun_checks/ch_resetup_state.py
+++ ch_tools/monrun_checks/ch_resetup_state.py
@@ -1,6 +1,4 @@
-import json
 import os
-import subprocess
 from typing import Any
 
 import click
@@ -11,17 +9,13 @@ from ch_tools.common.result import CRIT, OK, Result
 from ch_tools.monrun_checks.exceptions import die
 
 
-# TODO: delete unused ssl and ca_bundle options after some time
 @click.command("resetup-state")
-@click.option("-s", "--ssl", "_ssl", is_flag=True, help="Use HTTPS rather than HTTP.")
-@click.option("--ca_bundle", "_ca_bundle", help="Path to CA bundle to use.")
 def resetup_state_command(_ssl: bool, _ca_bundle: Any) -> Any:
     """
     Check state of resetup process.
     """
 
     check_resetup_running()
-    check_resetup_required()
 
     if os.path.isfile(CLICKHOUSE_RESETUP_CONFIG_PATH):
         return Result(
@@ -40,20 +34,3 @@ def check_resetup_running() -> None:
             die(0, "resetup is running (restore schema)")
         if {"/usr/bin/chadmin", "wait", "replication-sync"}.issubset(proc.cmdline()):
             die(0, "resetup is running (wait for replication sync)")
-
-
-def check_resetup_required() -> None:
-    """
-    Check resetup conditions
-    """
-    cmd = [
-        "sudo",
-        "salt-call",
-        "mdb_clickhouse.resetup_required",
-        "--out",
-        "json",
-        "--local",
-    ]
-    output = subprocess.check_output(cmd, stderr=subprocess.STDOUT)
-    if json.loads(output)["local"]:
-        die(0, "OK")
