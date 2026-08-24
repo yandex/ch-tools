4da5e63941c7eb7da71fe4fa0ea8aa6b5f33e92a
uber <uberdever@gmail.com>
2026-08-24 13:16:28 +0300

    use mntr for keeper readiness

--- tests/configuration.py
+++ tests/configuration.py
@@ -40,7 +40,8 @@ def create() -> dict:
             "healthcheck": {
                 "test": [
                     "CMD-SHELL",
-                    "echo ruok | nc 127.0.0.1 2181 | grep -q imok",
+                    "printf mntr | nc 127.0.0.1 2181 "
+                    "| grep -q '^zk_server_state[[:space:]]*leader$'",
                 ],
                 "interval": "500ms",
                 "timeout": "1s",
